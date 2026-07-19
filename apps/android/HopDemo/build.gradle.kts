c5295318a089d4c2200f18b466a3844f13b0d753
Jason Waldrip <jason@waldrip.net>
2026-07-19 00:06:12 -0600

    Merge pull request #237 from hopmesh/deps/android-toolchain-2026-07
    deps(android): AGP 9.3 + Gradle 9.6.1 + Kotlin 2.4.10 + core-ktx 1.19.0 (13 held bumps)
    
    GitOrigin-RevId: c1e3aaa72328ee0d9e75aaa9747fbf196ed8dfb4

--- apps/android/HopDemo/build.gradle.kts
+++ apps/android/HopDemo/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("com.android.application") version "9.2.1" apply false
-    id("com.android.library") version "9.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.4.0" apply false   // :hop-sdk (the sh.hop java-library)
+    id("com.android.application") version "9.3.0" apply false
+    id("com.android.library") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false   // :hop-sdk (the sh.hop java-library)
     // Kotlin 2.0+ moved the Jetpack Compose compiler out of the Kotlin compiler distribution + AGP's
     // composeOptions.kotlinCompilerExtensionVersion wiring and into this standalone Gradle plugin, pinned
     // to the Kotlin version. Applied (with a version) only in :app, the one module with @Composable code.
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
